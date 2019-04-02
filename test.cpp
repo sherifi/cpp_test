d109ed7738a53ba019e60ca128e7686dfc71b58b
sherifi <sherifi_co@yahoo.com>
2019-04-02 21:04:20 +0200

    Adding file to repository
    [Ticket: 1]

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace sherifi
+{
+	class Data_convertion
+	{
+	private:
+	protected:
+	public:
+		int x = 3;
+		double y = 5.2;
+		double z;
+	}
+}
+
+int main()
+{
+	sherifi::Data_convertion
+}
