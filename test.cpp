98a70438116a7004f81ff6dc9f0b41af64e4d342
sherifi <sherifi_co@yahoo.com>
2019-04-05 10:57:26 +0200

    Adding file
    Adding new and modified file
    
    [Ticket: X]

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits>
+#include <cstdlib>
 
 namespace sherifi
 {
@@ -8,12 +10,18 @@ namespace sherifi
 	protected:
 	public:
 		int x = 3;
-		double y = 5.2;
-		double z;
-	}
+		float y = 5.2;
+		char z = x + y;
+		unsigned short num = mumeric_limits <unsigned short> :: max();
+		short num2 = num;
+	};
 }
-
 int main()
 {
-	sherifi::Data_convertion
+	sherifi::Data_convertion data_1;
+	std::cout << data_1.z << std::endl;
+	sherifi::Data_convertion data_2;
+	std::cout << data_2.num << std::endl;
+	std::cout << data_2.num2 << std::endl;
+	return 0;
 }
