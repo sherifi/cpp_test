26a9270d8cec4be8d1c283ad8831bf69eb736998
sherifi <sherifi_co@yahoo.com>
2019-04-08 22:58:52 +0200

    Modifying file and add new one's
    [Ticket: X]

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <climits>
 #include <cstdlib>
 
 namespace sherifi
@@ -12,16 +13,29 @@ namespace sherifi
 		int x = 3;
 		float y = 5.2;
 		char z = x + y;
-		unsigned short num = mumeric_limits <unsigned short> :: max();
-		short num2 = num;
+		// unsigned short num = mumeric_limits <unsigned short> :: max();
+		// short num2 = num;
+		int var_2 = 45;
+		int* var_1;
+		// *var_1 = 60;
+		void func(int * arg_1 = &x)
+		{
+			std::cout << arg_1 << std::endl;
+		}
+
 	};
 }
 int main()
 {
-	sherifi::Data_convertion data_1;
-	std::cout << data_1.z << std::endl;
-	sherifi::Data_convertion data_2;
-	std::cout << data_2.num << std::endl;
-	std::cout << data_2.num2 << std::endl;
+	// sherifi::Data_convertion data_1;
+	// std::cout << data_1.z << std::endl;
+	// sherifi::Data_convertion data_2;
+	// std::cout << data_2.num << std::endl;
+	// std::cout << data_2.num2 << std::endl;
+	sherifi::Data_convertion data_3;
+	// std::cout << data_3.var_1->x << std::endl;
+	std::cout << data_3.func(4) << std::endl;
+
 	return 0;
 }
+
